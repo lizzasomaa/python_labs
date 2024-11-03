39feeedf95b9bd8b7df81292c9273df4c347af7c
lizzasomaa <lizzasomaa@yahoo.com>
2024-11-03 20:36:22 +0300

    Add files via upload

---
+++ лаб3/task3.py
@@ -0,0 +1,62 @@
+def count_letters(text):
+    text = text.lower()
+    dictionary = {}
+    for symbol in text:
+        if symbol.isalpha() == True:
+            if symbol in dictionary:
+                dictionary[symbol] += 1
+            else:
+                dictionary[symbol] = 1
+    return dictionary
+
+
+def calculate_frequency(dictionary):
+    total = 0
+    for letter in dictionary:
+        total += dictionary[letter]
+    for letter in dictionary:
+        dictionary[letter] /= total
+    return dictionary
+
+
+main_str = """
+У лукоморья дуб зелёный;
+Златая цепь на дубе том:
+И днём и ночью кот учёный
+Всё ходит по цепи кругом;
+Идёт направо — песнь заводит,
+Налево — сказку говорит.
+Там чудеса: там леший бродит,
+Русалка на ветвях сидит;
+Там на неведомых дорожках
+Следы невиданных зверей;
+Избушка там на курьих ножках
+Стоит без окон, без дверей;
+Там лес и дол видений полны;
+Там о заре прихлынут волны
+На брег песчаный и пустой,
+И тридцать витязей прекрасных
+Чредой из вод выходят ясных,
+И с ними дядька их морской;
+Там королевич мимоходом
+Пленяет грозного царя;
+Там в облаках перед народом
+Через леса, через моря
+Колдун несёт богатыря;
+В темнице там царевна тужит,
+А бурый волк ей верно служит;
+Там ступа с Бабою Ягой
+Идёт, бредёт сама собой,
+Там царь Кащей над златом чахнет;
+Там русский дух… там Русью пахнет!
+И там я был, и мёд я пил;
+У моря видел дуб зелёный;
+Под ним сидел, и кот учёный
+Свои мне сказки говорил.
+"""
+
+dictionary = calculate_frequency(count_letters(main_str))
+
+for letter, freq in dictionary.items():
+    print(f"{letter}: {freq:.2f}")
+
